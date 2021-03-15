4babdd40c94330a3966da0c893c27d4094a9fc6a
Mdehmoubed <55519800+Mdehmoubed@users.noreply.github.com>
2021-03-15 00:02:23 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-    // collapsible menu sections
+        // collapsible menu sections
     // collapsible menu sections
     var coll= document.getElementsByClassName("collapsible");
     for (var i = 0; i < coll.length; i++) {
@@ -12,28 +12,35 @@
             }
         });
     }
-   
-    var user
+    t1= new Date('06-24-2020');
+       
+    
     var groups
     var today= new Date();
     var month=today.getMonth();
     var year=today.getFullYear();
     var day =new Date(today.getTime() - (today.getTimezoneOffset() * 60000 )).toISOString().split('T')[0]
-    var first_day=6  //it causes the first day of week to become Sunday  
+     //it causes the first day of week to become Sunday  
+    
+    
+
     $(document).ready(function(){
         month=today.getMonth();
         year=today.getFullYear();
+         
         view();
         
         })
    
-   // here I add an event lisinter for first day of week   
-    $("[name='first_day']").click(function() {s=($(this).val());first_day=parseInt(s); view();});
+   // here I add event lisinters for changing first_day and time_interval on ViewSet and save it on backend by setUser  
+    $("[name='first_day']").click(function() {fd=parseInt(($(this).val()));user.ViewSet.first_day=fd;view(); setUser()});
+    $("[name='timeInt']").click(function() {ti=parseInt($(this).val());user.ViewSet.timeInterval=ti;view(); setUser()});
+
     // we should to save all setting parameters such as theme, first_day of week ,.. on back_end database 
     //after complete this part on front_end I will code for it
     
     function view(){
-        var data={'first_day':first_day ,'month':month+1 , 'year':year}
+        var data={'ViewSet':user.ViewSet,'month':month+1 , 'year':year}
         myJSON = JSON.stringify(data);
         $.post("/month_calendar",{'data': myJSON},function(resp){
             user=resp.data.user
@@ -50,19 +57,21 @@
             m=$('[class|="grid-col"]').length
             n=day_header.length
             for(i=0; i<m-n;i++){$('[class|="grid-col"]').eq(6).remove()}
-            for(i=0 ; i<n-m ; i++){$('[class|="grid-col"]').eq(0).after($('[class|="grid-col"]').eq(7).clone())}
-              
-                   
+            for(i=0 ; i<n-m ; i++){$('[class|="grid-col"]').eq(0).before($('[class|="grid-col"]').eq(6).clone())}
+           
+                      
+            //put day_headers on proper positions       
             for (i in day_header){
                 e=$('.date').eq(i)
                 e.html(day_header[i].name)
-                e.attr('id',day_header[i].id)
-                if (day==day_header[i].id) {e.parent().css('border','3px outset red')}
+                e.parent().attr('id',day_header[i].id)
+                if (day==day_header[i].id) {e.parent().css('border','3px outset red')}//for border of today
                 else {e.parent().css('border','')}
-                if (day_header[i].cur == 'c'){e.css("color", "#111111"); continue}
-                if (day_header[i].cur == 'n'){e.css("color", "#7fbf7f")}
+                if (day_header[i].cur == 'c'){e.css("color", "#111111"); continue} //current month color
+                if (day_header[i].cur == 'n'){e.css("color", "#7fbf7f")}  //previous and nex month color
                 else{e.css("color" ,"#7f7fbf")}
             }
+            //put day_event(s) on proper positions
             for (i in day_event){
                 e=$('.event-area').eq(i)
                 e.empty();
@@ -73,8 +82,48 @@
                     }
                 }
             }
+             //add listiner for each day 
+            $('[class|="grid-col"]').off()
+            $('[class|="grid-col"]').click(function(){
+                //write proper code for here when click alert just for test   
+                alert($(this).attr('id'))
+                })
+            
+            
+           
+
+            // making option for time slots
+            k=user.ViewSet.timeInterval
+            $('#start').html('<option>00:00</option>'); et1=$('#start').children().clone();$('#end').html('')
+            t1.setHours(0,0,0,0)
+            for (i=0 ;i<24*60;i+=k){
+                t1.setMinutes(t1.getMinutes()+k);p=t1.toTimeString().slice(0,5)
+                et1.html(p);et1.val(p);$('#start').append(et1.clone());$('#end').append(et1.clone())
+            }
+
+            //making options for reminder
+            var rem= ["Not Reminder","at time of Event","5 minute before","15 minute before","30 minute before" ,'1 hour before','2 hours before','1 Day before','1 Week before']
+            ind=$('#reminder')
+            ind.html('<option></option>');e=ind.children();ind.html('')
+            for (i in rem){
+                e.html(rem[i]);e.val(i)
+                ind.append(e.clone())
+                }
+           
+            
+            
+                
+
+            a='#startWeek > #startWeek'+user.ViewSet.first_day.toString() 
+            $(a).prop('checked',true);
+            a='#timeSlot > #t'+user.ViewSet.timeInterval.toString() 
+            $(a).prop('checked',true);
+
+
             e=$('[id|="group"]').eq(0);  m=$('[id|="grList"]').eq(0); 
             $('[id|="group"]').remove();   $('[id|="grList"]').remove()
+
+
             
             for (i in groups){
                 gr=groups[i];
@@ -110,10 +159,10 @@
        var a= ($('#start').val()=="")||($('#end').val()=="")||($('#date').val()=="")||($('#title').val()=="")
        //add some code to check overlaping time ,....
        if (a) {alert ('some fild doesnt fill'); return}
-       $('#addEventBtn').prop('disabled',true)
+       $('#addEventBtn').prop('disabled',true);reminder=$('#reminder').val()
        title=$('#title').val(); address=$('#descr').val() ; date=$('#date').val()
        start=$('#start').val(); end=$('#end').val();groupID=$('#group_list').val()
-       var send={'st':'addEv','name':title , 'address':address ,'date':date ,'start':start,'end':end,'groupID':groupID}
+       var send={'st':'addEv','name':title , 'address':address ,'date':date ,'start':start,'end':end,'groupID':groupID,'reminder':reminder}
            myJSON = JSON.stringify(send);
            $.post( "/edit_event", {'id': myJSON},function(data){
                 for (i in day_header){
@@ -198,4 +247,21 @@
         myJSON = JSON.stringify(send);
         $.post( "/edit_group", {'id': myJSON},function(data){view()})
     }
-   
+    function changeEnd(){
+        k=user.ViewSet.timeInterval
+        $('#end').html(''); et1=$('#start').children().eq(0).clone()
+        st=$('#start').val()
+        t1.setHours(st.slice(0,2),st.slice(3,5),0,0)
+        h=parseInt(st.slice(0,2))*60+parseInt(st.slice(3,5))
+        //alert(i)
+        for (i=0 ;i<24*60-h-k;i+=k){
+            t1.setMinutes(t1.getMinutes()+k);p=t1.toTimeString().slice(0,5)
+            et1.html(p);et1.val(p);$('#end').append(et1.clone()) }
+            et1.html('23:59');et1.val("23:59");$('#end').append(et1.clone()) 
+    }
+    
+    function setUser(){
+        var send={'st':'save_user'}
+        myJSON = JSON.stringify(send);
+        $.post( "/edit_group", {'id': myJSON},function(data){view()})
+    }
