cc53760f5bc3df690c7ca282407b379dcd084f27
tktedder <60683140+tktedder@users.noreply.github.com>
2021-04-14 14:25:13 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,46 @@
-from flask import Flask, render_template, request, session ,url_for,redirect,flash ,logging ,jsonify,json
+from flask import Flask, render_template, request, session , url_for, redirect, flash, logging, jsonify, json
 from flask_mail import Mail, Message
 from flask_apscheduler import APScheduler
 from threading import Thread
 import sqlite3 as sql
 from functools import wraps
-from datetime import date,time,datetime,timedelta
+from datetime import date, time, datetime, timedelta
 import calendar
 import random
+import time
 
-
-
-#from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
 
 DATABASE ='/mdn1.db'
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-###this part for scheduler
+
+# this part for scheduler
 scheduler = APScheduler()# scheduler object for reminder email
 scheduler.init_app(app) # init it whit app
 scheduler.start() # start to work
 
 
-##### this part for email sender 
+# this part for email sender 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'procalendar.mehrdad@gmail.com'
 app.config['MAIL_PASSWORD'] = 'Nasrin@1234'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-#manager = Manager(app)
 mail = Mail(app)
-def shell_context():
-    import os, sys
-    return dict(app=app, os=os, sys=sys)
 
-#manager.add_command("shell", Shell(make_context=shell_context))
 
 def async_send_mail(app, msg):
     with app.app_context():
         mail.send(msg)
 
+        
 
 
-def send_email(email_address,message_body,subjects):
+def send_email(email_address, message_body, subjects):
     msg = Message(subjects, sender = 'procalendar.mehrdad@gmail.com', recipients = [email_address])
     msg.body = message_body
     thr = Thread(target=async_send_mail, args=[app, msg])
@@ -53,9 +49,13 @@ def send_email(email_address,message_body,subjects):
 
 
 
+
 @app.route('/')
 def home():
     return render_template('home1.html')
+
+
+
 
 def is_logged_in(f):
     @wraps(f)
@@ -63,14 +63,18 @@ def is_logged_in(f):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            #flash( Please login')
             return redirect(url_for('no'))
     return wrap
+
 
 
 @app.route('/register')
 def ne():
     return render_template('register.html',msg='')#chang with flash 
+
+
+
+
 @app.route('/aduser', methods=['POST','GET'])
 def adduser():
     if request.method =='POST':
@@ -97,13 +101,11 @@ def adduser():
         finally:
             con.close()
             if msg=='' :
-                #return render_template('account.html',username =uname)
                 subjects='Welcome to Pro-Calendar '
                 message_body=f"Hello {fname},\n You are a Proclander   "
                 email_address=iemail
                 send_email(iemail,message_body,subjects)
                 return redirect('/login')
-                
             else:
                 print(msg)
                 return render_template('register.html',msg = msg)
@@ -111,13 +113,15 @@ def adduser():
         return     
 
 
-    
 
 
-#login
 @app.route('/login')
 def no():
     return render_template('login.html',msg='')#change with flash 
+
+
+
+
 @app.route('/login1', methods=['POST','GET'])
 def logi():
     if request.method =='POST':
@@ -133,7 +137,7 @@ def logi():
             if retrive[0]['password'] == psw :
                session['logged_in'] = True
                session['username'] = uname 
-              # print("ok"+ str(retrive[0]['password']))
+            # print("ok"+ str(retrive[0]['password']))
                return redirect(url_for('dashboard')) 
             else :
                 error ='invalid login'
@@ -146,10 +150,12 @@ def logi():
         return                
 
 
-##dashboard
+
+
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():   
+    time.sleep(0.1)
     con=sql.connect("mdn1.db")
     con.row_factory =dict_factory
     cur=con.cursor()
@@ -162,21 +168,21 @@ def dashboard():
     groups=cur.fetchall()
     con.close()
     if user['ViewSet']['viewLy']=='week':
-        print('weeeeeeeeeeeeeeeeeek')
-        return render_template('weekly.html',user=user)
+        return render_template('weekly.html', user=user)
     if user['ViewSet']['viewLy']=='year':
-        return render_template('yearly.html',user=user)
+        return render_template('yearly.html', user=user)
     if user['ViewSet']['viewLy']=='day':
-        return render_template('dayly.html',user=user)        
-    return render_template('monthly.html',user=user)
+        return render_template('daily.html', user=user)
+    if user['ViewSet']['viewLy']=='account':
+        return render_template('account.html', user=user)
+    return render_template('monthly.html', user=user)
 
 
-## month calendar MVC 
-@app.route('/month_calendar',methods = ['POST','GET'])
+
+
+@app.route('/month_calendar', methods = ['POST','GET'])
 @is_logged_in
-def month_calendar():
-   
-#   if method != 'POST" return redirect 
+def month():  
     data_rec=json.loads(request.form['data'])   
     month = data_rec['month']
     year = data_rec['year']
@@ -184,13 +190,12 @@ def month_calendar():
     global ViewSet
     global user
     ViewSet = data_rec['ViewSet']
-    user['ViewSet']=ViewSet
+    user['ViewSet'] = ViewSet
     fd=int(ViewSet['first_day'])
     week_header,week_abbr,month_name,month_abbr,c = calendName(fd)
     month_header=month_name[month]+" "+str(year)
-
-#make day_header {0:{name:28,cur:c}}  p/c/n are the posible value for cur
-# maybe we want to show them in another style
+    # make day_header {0:{name:28,cur:c}}  p/c/n are the posible value for cur
+    # maybe we want to show them in another style
     next_month=((month)%12)+1
     pre_month=((month-2)%12)+1 
     day_header=[]
@@ -218,17 +223,15 @@ def month_calendar():
             event.date =? and event.username=? order by event.start",(day_id,uname))
         day_event.append(cur.fetchall())
     con.close()  
-    #print (day_event)
-    data={'month_header':month_header,'week_header':week_header,'user':user,\
-    'day_header':day_header,'day_event':day_event,'groups':groups}
-
+    data = {'month_header':month_header, 'week_header':week_header, 'user':user, 'day_header':day_header, 'day_event':day_event, 'groups':groups}
     return {'data':data}
+
+
+
 
 @app.route('/week_calendar',methods = ['POST','GET'])
 @is_logged_in
-def week_calendar():
-   
-#   if method != 'POST" return redirect 
+def week():
     data_rec=json.loads(request.form['data'])   
     month = data_rec['month']
     year = data_rec['year']
@@ -249,13 +252,13 @@ def week_calendar():
     for i in c.itermonthdays4(year,month):
         if(st<=0 and st>-7):
             hd=month_abbr[i[1]]+' '+str(i[2])
-            wekk.append({'week_head':hd,'id':date(i[0],i[1],i[2]).strftime('%Y-%m-%d'),'weekS':week_abbr[i[3]]+' '+hd})
+            wekk.append({'week_head':hd, 'id':date(i[0],i[1],i[2]).strftime('%Y-%m-%d'), 'weekS':week_abbr[i[3]] + ' ' + hd})
         st-=1
-    #print(wekk)
     if wekk[0]['week_head'].split(' ')[0]==wekk[-1]['week_head'].split(' ')[0]:
-        head_cal_week=wekk[0]['week_head']+'-'+wekk[-1]['week_head'].split(' ')[1]+','+str(year)
+        head_cal_week=wekk[0]['week_head'] + '-' + wekk[-1]['week_head'].split(' ')[1] + ', ' + wekk[-1]['id'].split('-')[0]
     else:
-        head_cal_week=wekk[0]['week_head']+'-'+wekk[-1]['week_head']+','+str(year)
+        head_cal_week=wekk[0]['week_head'] + '-' + wekk[-1]['week_head'] + ', ' + wekk[-1]['id'].split('-')[0]
+    print(wekk)
     k=ViewSet['timeInterval']
     t1= datetime(2020,2,2,0,0)
     td=timedelta(minutes=ViewSet['timeInterval'])
@@ -277,32 +280,28 @@ def week_calendar():
             order by event.start",(j['id'],uname,ta,tb,ta,tb,ta,ta))
             time_event.append(cur.fetchall())
         t1+=td
-    #print(time_header)
-    #print(time_event)
-    
     con.close()  
-    
-    data = {'head_cal_week':head_cal_week,'wekk':wekk,'user':user,'time_header':time_header,'time_event':time_event,'groups':groups}
+    data = {'head_cal_week':head_cal_week, 'wekk':wekk, 'user':user, 'time_header':time_header, 'time_event':time_event, 'groups':groups}
     return {'data':data}
 
 
 
+
 def calendName(fd):
-    week_name={0:'MONDAY',1:'TUESDAY',2:'WEDNESDAY',3:'THURSDAY',4:'FRIDAY',5:'SATURDAY',6:'SUNDAY'}
+    week_name={0:'MONDAY', 1:'TUESDAY', 2:'WEDNESDAY', 3:'THURSDAY', 4:'FRIDAY', 5:'SATURDAY', 6:'SUNDAY'}
     month_name=[]
     for i in calendar.month_name:
         month_name.append(i)
-# month abbr 
     month_abbr=[]
     for i in calendar.month_abbr:
         month_abbr.append(i)
-#this is first day of week 0=monday 6=sunday 5=saturday 
+    # this is first day of week 0=monday 6=sunday 5=saturday 
     c=calendar.Calendar(firstweekday=fd)
-#make proper week header (to make a list)
+    # make proper week header (to make a list)
     week_header=[]
     for i in c.iterweekdays():
         week_header.append(week_name[i]) 
-#make week abbr
+    # make week abbr
     week_abbr=[]
     for i in calendar.day_abbr:
         week_abbr.append(i) 
@@ -310,17 +309,16 @@ def calendName(fd):
 
 
 
-##logout
+
 @app.route('/logout')
 @is_logged_in
 def logout():
     session.clear()
-    #flash
     return redirect('/login')
 
 
 
-#checkuser
+
 @app.route('/usercheck',methods = ['POST','GET']) 
 def checkuser():
     con=sql.connect("mdn1.db")
@@ -336,8 +334,6 @@ def checkuser():
         cur.row_factory= dict_factory
         cur.execute('SELECT * FROM users WHERE email=?',(checkReq['iemail'],))
         a=cur.fetchall()
-        #print(checkReq['iemail'])
-        #print(a)
         con.close()
         return {'pass':(len(a)==0 )}
     if checkReq['flag'] == 'validemail':
@@ -353,7 +349,6 @@ def checkuser():
        global requestTime 
        requestTime = datetime.now()
        print(requestTime) 
-       timedelta(minutes=1)
        requestTime += timedelta(minutes=1)
        print(requestTime)
        return  'ok'  
@@ -361,8 +356,9 @@ def checkuser():
         code= int(checkReq['code'])
         allPass = datetime.now() < requestTime and code == rndNum
         return{'pass': allPass }
-    
-## for del and edit and add events
+
+
+
 
 @app.route('/edit_event', methods = ['POST','GET'])
 @is_logged_in
@@ -370,8 +366,6 @@ def edit_event():
     jsd = json.loads(request.form['id'])
     con=sql.connect("mdn1.db")
     cur=con.cursor()
-    #print(jsd)
-
     if(jsd['st'] =='editEv' or jsd['st'] =='addEv'):
         name,address=jsd['name'],jsd['address']
         uname=session['username']
@@ -380,30 +374,23 @@ def edit_event():
         endt=jsd['endt']
         groupID =jsd['groupID']
         reminder=jsd['reminder']
-        #print(name,address,uname,date,start,endt,groupID)
-    if (jsd['st'] =='edit'):
-        event_id=int(jsd['ido'])
-        cur.execute('UPDATE event SET eventname=?,address=?,username=?,date=?,\
-                    start=?,endt=?,group=? reminder=? WHERE eventID=?',(name,address,uname,date,start,endt,groupID,reminder,event_id))
-        scheduler.remove_job(str(event_id))
-   
-    if (jsd['st']=='addEv'):
-        cur.execute('INSERT INTO event(eventname,address,username,date,start,endt,groupID,reminder)\
-                    VALUES (?,?,?,?,?,?,?,?)',(name,address,uname,date,start,endt,groupID,reminder))
-        email=user['email']
-        fname=user['fname']
-        subjects='PRO-Calendar You Add an event'
-        message_body= 'Hello '+fname+', \n You add an event to your pro_calendar.\n The detail is:\n EVENT NAME:\t'\
-        +name+'\n ADDRESS:\t'+address+'\n Event Date:\t'+str(date)+'\n Start Time:\t'+str(start)\
-        +'\n End Time:\t'+str(endt)+'\n \n Pro-claenar team'
-        send_email(email,message_body,subjects)
-        
-    
-    if (jsd['st']=='delEv'):
-        a=int(jsd['ido'])
-        cur.execute('DELETE FROM event WHERE eventID = ?;',(a,))
-        scheduler.remove_job(str(a))
-    
+        if (jsd['st'] =='edit'):
+            event_id=int(jsd['ido'])
+            cur.execute('UPDATE event SET eventname=?, address=?, username=?, date=?, start=?, endt=?, group=? reminder=? WHERE eventID=?', (name, address, uname, date, start, endt, groupID, reminder, event_id))
+            scheduler.remove_job(str(event_id))
+        if (jsd['st']=='addEv'):
+            cur.execute('INSERT INTO event(eventname, address, username, date, start, endt, groupID, reminder)\VALUES (?,?,?,?,?,?,?,?)', (name, address, uname, date, start, endt, groupID, reminder))
+            email=user['email']
+            fname=user['fname']
+            subjects='PRO-Calendar You Add an event'
+            message_body= 'Hello' + fname + ',\n You add an event to your pro_calendar.\n The detail is:\n EVENT NAME:\t'\
+             + name + '\n ADDRESS:\t' + address + '\n Event Date:\t' + str(date) + '\n Start Time:\t' + str(start)\
+             + '\n End Time:\t' + str(endt) + '\n\n Pro-claenar team'
+            send_email(email,message_body,subjects)
+        if (jsd['st']=='delEv'):
+            a=int(jsd['ido'])
+            cur.execute('DELETE FROM event WHERE eventID = ?;',(a,))
+            scheduler.remove_job(str(a))
     con.commit()
     con.close()
     con=sql.connect("mdn1.db")
@@ -413,63 +400,50 @@ def edit_event():
         date='2021-03-02'
     cur.execute("select * from event where username = ? and date = ? Order by date,start;",(session['username'],date))
     events =cur.fetchall()
-
     #add_sch is not in proper place
-    add_sch(events,user)
+    add_sch(events,user)    
     con.close()
     day_data={'day_event':events,'day_id':date}
     return {'day_data':day_data}
 
 
 
+
 @app.route('/edit_group', methods = ['POST','GET'])
 @is_logged_in
-def edit_group():    
+def edit_group():
     jsd = json.loads(request.form['id'])
     con=sql.connect("mdn1.db")
     cur=con.cursor()
-       
     if(jsd['st'] =='editGr' or jsd['st'] =='addGr'):
         name,color=jsd['name'],jsd['color']
-        uname,showG=session['username'],True
-        #print(name,color,uname,showG)
-    
+        uname,showG=session['username'],True    
     if (jsd['st'] =='editGr'):
         groupID=int(jsd['groupID'])
         cur.execute('UPDATE groups SET name=?,color=?,username=?,showG=? WHERE groupID=?',(name,color,uname,showG,groupID))
-   
     if (jsd['st']=='addGr'):
         cur.execute('INSERT INTO groups(name,username,color,showG) VALUES (?,?,?,?)',(name,uname,color,showG))
-        ##for send email
+        # for send email
         con.commit()
         cur.row_factory= dict_factory
         cur.execute('SELECT * FROM groups WHERE username=?',(uname,))
         a=cur.fetchall()
-
     if (jsd['st']=='showChange'):        
         cur.execute('UPDATE groups SET showG=? WHERE groupID=?',(jsd['showG'],int(jsd['id'])))
-
     if (jsd['st']=='delGr'):
         a=int(jsd['id'])
-        #print(a)
         cur.execute('DELETE FROM groups WHERE groupID = ?;',(a,))
-    
     if (jsd['st']=='save_user'):
-        #print(jsd)
         global user
         user=jsd['user']
-        #password=user['password']
         lname=user['lname']
         fname=user['fname']
         ViewSet=user['ViewSet']
-        #print(ViewSet)
         ViewSet=json.dumps(ViewSet)
         uname=user['username']
         print(ViewSet)
         print(uname)
         cur.execute('UPDATE users SET lname=?,fname=?, ViewSet=? WHERE username=?',(lname,fname,ViewSet,uname))
- 
-     
     con.commit()
     con.close()
     con=sql.connect("mdn1.db")
@@ -483,7 +457,8 @@ def edit_group():
 
 
 
-def add_sch(events,user):
+
+def add_sch(events, user):
     for event in events :
         ind= event['reminder']
         if ind==0 :
@@ -499,20 +474,18 @@ def add_sch(events,user):
         scheduler.add_job(func=scheduled_task, trigger='date', run_date=r_d,args=[event,user,e_id], id=e_id)
     return
 
-def scheduled_task(event,user,e_id):
-    message_body=user['fname']+', you have an upcomming event\n on '+event['date']+' ,at '+event['start']+'\n pro-calendar team'
+
+
+
+def scheduled_task(event, user, e_id):
+    message_body=user['fname'] + ', you have an upcomming event\n on ' + event['date'] + ' ,at ' + event['start'] + '\n\
+    The event name is ' + event['eventname'] + '\n\n pro-calendar team'
     subjects='reminder'
-    
-   
+    print(event['eventID'])
     send_email(user['email'],message_body,subjects)
     return
 
-    
 
-
-
-#def add_sch(email,message_body,subjects,date,start,event_id):
-#    return
 
 
 def dict_factory(cursor, row):
@@ -522,6 +495,7 @@ def dict_factory(cursor, row):
     return d
 
 
+
+
 if '__name__'=='__main__':
-    
     app.run(debug=True)
